d9b5ef00be91a6e7e58044c5fad6dbdae53f7d68
Sivabalan Muthurajan <sivabalan.muthurajan@botpress.com>
2026-02-10 16:17:26 -0500

    Add keybinds to create and rename files

--- dot_config/nvim/lua/keybinds.lua
+++ dot_config/nvim/lua/keybinds.lua
@@ -35,6 +35,38 @@ vim.keymap.set("n", "<Leader>F", function() vim.ui.input({ prompt = 'Filetype: '
 -- Save
 vim.keymap.set("n", "zz", ":update<CR>")
 
+-- Create new files in current buffer directory
+vim.keymap.set("n", "<Leader>N", function() 
+        local dir = vim.fn.expand("%:h")
+        local name = vim.fn.input("New file in " .. dir .. "/: ")
+        if name == "" then return end
+        vim.cmd("edit " .. vim.fn.fnameescape(dir .. "/" .. name))
+end, { desc = "New file in current buffer directory" })
+
+-- Rename files in current buffer directory
+vim.keymap.set("n", "<Leader>R", function()
+        local oldPathFileName = vim.fn.expand("%:p")
+        if oldPathFileName == "" then return end
+
+        local dir = vim.fn.fnamemodify(oldPathFileName, ":h")
+        local oldFileName = vim.fn.fnamemodify(oldPathFileName, ":t")
+
+        local newFileName = vim.fn.input("New file name for " .. oldFileName .. ": ", oldPathFileName, "file")
+        if newFileName == "" or newFileName == oldFileName then return end
+
+        if vim.bo.modified then vim.cmd("write") end
+
+        local ok, err = os.rename(oldPathFileName, newFileName)
+
+        if not ok then
+                vim.notify("Rename failed: " .. (err or ""), vim.log.levels.ERROR)
+                return
+        end
+
+        vim.cmd("edit " .. vim.fn.fnameescape(newFileName))
+        vim.cmd("bdelete!" .. vim.fn.fnameescape(oldPathFileName))
+end, { desc = "Rename current file to" })
+
 
 -- Command mode
 
@@ -43,4 +75,6 @@ vim.keymap.set("c", "<C-k>", "<Up>")
 vim.keymap.set("c", "<C-j>", "<Down>")
 vim.keymap.set("c", "<C-h>", "<Left>")
 vim.keymap.set("c", "<C-l>", "<Right>")
+
+
 
